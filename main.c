933421492d2ee95788ee57d51807ecbdef0f7c10
JCoMcL <rapcyss@gmail>
2020-09-01 16:00:32 +0100

    make cool style change

--- main.c
+++ main.c
@@ -3,25 +3,15 @@
 #include <time.h>
 int clearscreen();
 int timeout();
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 	struct winsize sz;
 
 	ioctl(0, TIOCGWINSZ, &sz);
 	printf("Screen width: %i  Screen height: %i\n", sz.ws_col, sz.ws_row);
-	clearscreen();
-	printf("A\n");
-	clearscreen();
-	timeout();
-	printf("B\n");
-	timeout();
-	clearscreen();
-	printf("C\n");
 	return 0;
 }
 
-int timeout()
-{
+int timeout() {
 	struct timespec tim,rem;
 	tim.tv_nsec=500000000L;
 	tim.tv_sec=0;
@@ -29,8 +19,7 @@ int timeout()
 	return 0;	
 }
 
-int clearscreen()
-{
+int clearscreen() {
 	printf("\e[1;1H\e[2J");
 	return 0;
 }
