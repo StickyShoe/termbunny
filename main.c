2eef1759c35b8777b62074de89307a6c560e83b0
JCoMcL <rapcyss@gmail>
2020-09-01 16:03:11 +0100

    remove uncool function headers and move main() to bottom

--- main.c
+++ main.c
@@ -1,15 +1,6 @@
 #include <sys/ioctl.h>
 #include <stdio.h>
 #include <time.h>
-int clearscreen();
-int timeout();
-int main(int argc, char **argv) {
-	struct winsize sz;
-
-	ioctl(0, TIOCGWINSZ, &sz);
-	printf("Screen width: %i  Screen height: %i\n", sz.ws_col, sz.ws_row);
-	return 0;
-}
 
 int timeout() {
 	struct timespec tim,rem;
@@ -21,5 +12,13 @@ int timeout() {
 
 int clearscreen() {
 	printf("\e[1;1H\e[2J");
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	struct winsize sz;
+
+	ioctl(0, TIOCGWINSZ, &sz);
+	printf("Screen width: %i  Screen height: %i\n", sz.ws_col, sz.ws_row);
 	return 0;
 }
